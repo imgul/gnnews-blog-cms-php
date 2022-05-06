cdb6b8d5e85ffe2e57a694e341b221ddb1f6eb3d
imgul <bc210411296@vu.edu.pk>
2022-05-06 21:52:19 +0500

    Change user role from profile edit settings

--- admin/users-profile.php
+++ admin/users-profile.php
@@ -147,6 +147,7 @@ if ((isset($_POST['update_user'])) && ($_SERVER['REQUEST_METHOD'] == 'POST')) {
     $up_username = test_input($_POST['username']);
     $up_email = test_input($_POST['email']);
     $up_about = test_input($_POST['about']);
+    $up_user_role = test_input($_POST['user_role']);
     // $password = test_input($_POST['password']);
     // $confirmPassword = test_input($_POST['confirmPassword']);
     // $alert_info = $firstname . $lastname . $username . $email . $password . $confirmPassword;
@@ -175,7 +176,7 @@ if ((isset($_POST['update_user'])) && ($_SERVER['REQUEST_METHOD'] == 'POST')) {
             // } else {
             // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
             // $queryUpdate = "INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `email`, `password`) VALUES (NULL, '$username', '$firstname', '$lastname', '$email', '$hashed_password');";
-            $queryUpdate = "UPDATE `users` SET `username` = '$up_username', `first_name` = '$up_first_name', `last_name` = '$up_last_name', `email` = '$up_email', `about` = '$up_about', `profile_pic` = '$target_file' WHERE `users`.`id` = '$user_id';";
+            $queryUpdate = "UPDATE `users` SET `username` = '$up_username', `first_name` = '$up_first_name', `last_name` = '$up_last_name', `email` = '$up_email', `about` = '$up_about', `profile_pic` = '$target_file', `role` = '$up_user_role' WHERE `users`.`id` = '$user_id';";
             echo "<meta http-equiv='refresh' content='0'>";
             // header("Refresh: 0");
             $resultUpdate = mysqli_query($conn, $queryUpdate);
@@ -258,18 +259,42 @@ include 'includes/navbar.php';
                                             <input class="d-none" type="file" accept="image/*" name="profileImg" id="profileImg__input" oninput="profileImg__img.src=window.URL.createObjectURL(this.files[0])">
                                         </div>
                                     </div>
-                                    <div class="col-sm-6 col-md-6">
+                                    <div class="col-sm-6 col-md-4">
                                         <div class="mb-4">
                                             <label for="username" class="form-label">Username</label>
                                             <input name="username" id="username" class="form-control" type="text" placeholder="Username" value="<?php echo $username; ?>" minlength="3" maxlength="20" required>
                                             <div class="invalid-feedback">Please enter username(3-20 Characters).</div>
                                         </div>
                                     </div>
-                                    <div class="col-sm-6 col-md-6">
+                                    <div class="col-sm-6 col-md-4">
                                         <div class="mb-4">
                                             <label for="email" class="form-label">Email address</label>
                                             <input name="email" id="email" class="form-control" type="email" placeholder="Email" value="<?php echo $email; ?>" required>
                                             <div class="invalid-feedback">Please enter your valid email.</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-4">
+                                        <div class="mb-4">
+                                            <label for="user-role" class="form-label">User Role</label>
+                                            <select name='user_role' class="form-select" id="user-role" aria-label="Default select example">
+                                                <?php
+                                                // Array for user roles
+                                                $user_roles = ['administrator', 'editor', 'author', 'contributor', 'subscriber'];
+                                                // Loop through the array
+                                                foreach ($user_roles as $user_role) {
+                                                    $user_role_capital = ucfirst($user_role);
+                                                    // If the current role is equal to the user role
+                                                    if ($user_role == $role) {
+                                                        // Display the current role as selected
+                                                        echo "<option value='$user_role' selected>$user_role_capital</option>";
+                                                    } else {
+                                                        // Display the current role
+                                                        echo "<option value='$user_role'>$user_role_capital</option>";
+                                                    }
+                                                }
+
+                                                ?>
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="col-sm-6 col-md-6">
@@ -307,10 +332,10 @@ include 'includes/navbar.php';
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start fw-bold">
-                        <p class="mb-2 mb-md-0 fw-bold">Your company &copy; 2022</p>
+                        <p class="mb-2 mb-md-0 fw-bold">Your company &copy; <?php echo date('Y'); ?></p>
                     </div>
                     <div class="col-md-6 text-center text-md-end text-gray-400">
-                        <p class="mb-0">Version 1.3.0</p>
+                        <p class="mb-0">Version 1.0.0</p>
                     </div>
                 </div>
             </div>
